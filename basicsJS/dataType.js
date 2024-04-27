52d47697b1b237e5d815aa7e3e9b5c40cfe536db
ganeshnarangle-git <168304582+ganeshnarangle-git@users.noreply.github.com>
2024-04-27 20:36:57 +0000

    datatype

---
+++ basicsJS/dataType.js
@@ -0,0 +1,21 @@
+
+
+let a= 12;
+let name = 'Ganesh';
+let isValid = false;
+let state; // null
+
+
+
+// number = 2 to 2 power 53
+// bigint
+//string
+//boolean 
+//null = empty , ex temp= 0
+// undefined , no values assign
+
+
+// object 
+
+console.log(typeof "Om");
+console.log(typeof null);
